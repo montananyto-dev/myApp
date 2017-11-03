3d483d2cbb72d8c6b0d3121b4d6b3300b4126452
Tony Mace <k1451757@kingston.ac.uk>
2017-11-03 20:03:45 +0000

    Show or hide all users on the home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/users/user.service';
 
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,13 +11,20 @@ import { UserService } from '../../services/users/user.service';
 })
 export class HomeComponent implements OnInit {
 
+  allUsers;
+  displayUsers = false;
 
   constructor(private user:UserService) {
 
+    this.allUsers = user.getAllUsers();
     
    }
 
   ngOnInit() {
   }
 
+  showUsers(){
+    this.displayUsers = !this.displayUsers;
+    
+  }
 }
